ade63dcc3c37f37cdfa7f43bee99ec251d513728
João Pedro Della Rocca de Camargos <joao.camargos@estudante.ifb.edu.br>
2024-02-09 16:48:45 -0300

    fix: fixing mock import

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Header from "./components/Header/Index";
 import Main from "./components/Main/Index";
-import data from "./mocks/Data";
+import data from "./mocks/data.js";
 import Footer from "./components/Footer/Index";
 
 export default function App() {
